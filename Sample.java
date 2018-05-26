a0c12e5b2f223d4cc6f1b64feb1b3b883ec7ae54
Arulprasath <arulprasath36@gmail.com>
2018-05-26 19:08:29 +0530

    Create Sample.java

---
+++ Sample.java
@@ -0,0 +1,10 @@
+public class HelloGuys {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hey Agni");
+		System.out.println("Hey Riya");
+		System.out.println("Hey Duck");
+	}
+
+}
